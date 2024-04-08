8b948be9b56431124acb7a27878ff848364a0856
kamesh0407 <kameshragupathi6@gmail.com>
2024-04-08 15:22:55 +0530

    front end design completed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from "react";
 const Message = () => {    
     return(
         <div>
-            <div className="message">
+            <div className="message owner">
                 <div className="messageInfo">
                     <img src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                     <span>Just now</span>
                 </div>
                 <div className="messageContent">
                     <p>Hello</p>
-                    {/* <img src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" /> */}
+                    <img src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                 </div>
 
 
